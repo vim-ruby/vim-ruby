121c2f6c1e9bda3f524f66cc9a9cc8e0c982597d
Gavin Sinclair <gsinclair@gmail.com>
2003-07-27 08:42:14 +0000

    Initial revision

---
+++ build.rb
@@ -0,0 +1,13 @@
+#!/usr/local/bin/ruby -w
+
+# This program creates a tarball which people can download and install whereever
+# they like, so that they can get the tasty goodness without having to use CVS
+# and install.rb.
+
+# How can we make it easy to build using different CVS tags?
+
+USAGE = <<-EOF
+Usage: ruby build.rb
+EOF
+
+raise "Not implemented"
