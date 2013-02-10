vim-ruby/vim-ruby

commit 0c4190c1c88e25fcd2994709da8c27f22dd697ec

Fix bug with interpolation in %|| strings

indent/ruby.vim
@@ -34,7 +34,7 @@ set cpo&vim
 
 " Regex of syntax group names that are or delimit string or are comments.
 let s:syng_strcom = '\<ruby\%(Regexp\|RegexpDelimiter\|RegexpEscape' .
-      \ '\|String\|StringEscape\|ASCIICode' .
+      \ '\|String\|StringDelimiter\|StringEscape\|ASCIICode' .
       \ '\|Interpolation\|NoInterpolation\|Comment\|Documentation\)\>'
 
 " Regex of syntax group names that are strings.
@@ -498,12 +498,15 @@ function GetRubyIndent(...)
 
   " If the previous line ended with [*+/.,-=], but wasn't a block ending,
   " indent one extra level.
-  if s:Match(lnum, s:non_bracket_continuation_regex) && !s:Match(lnum, '^\s*\(}\|end\)')
+  if s:Match(lnum, s:non_bracket_continuation_regex)
+        \ && !s:Match(lnum, '^\s*\(}\|end\)')
+        \ && !s:IsInStringOrComment(lnum, len(line))
     if lnum == p_lnum
       let ind = msl_ind + &sw
     else
       let ind = msl_ind
     endif
+    return ind
   endif
 
   " }}}2

etc/examples/indent/strings.rb
@@ -1,0 +1,2 @@
+command = %|#{file}|
+settings.log.info("Returning: #{command}")

commit 96b308b9a2cd99d198e26b51b79d9c3930d9b789

Remove unnecessary check
This fixes a regression with case statements from commit
0c4190c1c88e25fcd2994709da8c27f22dd697ec.

Turns out, `s:Match` already handles the string-or-comment check, and in
a much smarter way, so the abovementioned commit only needed to update
the syntax groups checked for being a "string".

indent/ruby.vim
@@ -498,9 +498,7 @@ function GetRubyIndent(...)
 
   " If the previous line ended with [*+/.,-=], but wasn't a block ending,
   " indent one extra level.
-  if s:Match(lnum, s:non_bracket_continuation_regex)
-        \ && !s:Match(lnum, '^\s*\(}\|end\)')
-        \ && !s:IsInStringOrComment(lnum, len(line))
+  if s:Match(lnum, s:non_bracket_continuation_regex) && !s:Match(lnum, '^\s*\(}\|end\)')
     if lnum == p_lnum
       let ind = msl_ind + &sw
     else