76384e121dde94369e4357ab58d897f1b7c23cec
Andrew Radev <andrey.radev@gmail.com>
2013-10-03 09:27:19 +0200

    Tests for string interpolation problem

--- spec/indent/continuations_spec.rb
+++ spec/indent/continuations_spec.rb
@@ -68,10 +68,31 @@ describe "Indenting" do
   end
 
   specify "string interpolation" do
-    # See https://github.com/vim-ruby/vim-ruby/issues/93 for details
+    # For details, see:
+    #
+    #   https://github.com/vim-ruby/vim-ruby/issues/93
+    #   https://github.com/vim-ruby/vim-ruby/issues/160
+    #
     assert_correct_indenting <<-EOF
       command = %|\#{file}|
       settings.log.info("Returning: \#{command}")
+    EOF
+
+    assert_correct_indenting <<-EOF
+      {
+        thing: "[\#{}]",
+        thong: "b"
+      }
+    EOF
+
+    assert_correct_indenting <<-EOF
+      {
+        a: "(\#{a})",
+        b: "(\#{b})",
+        c: "(c)",
+        d: "(d)",
+        e: "(e)",
+      }
     EOF
   end
 
