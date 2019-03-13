4abc4340abff09698fc5c8fe91dec87e3c3e2255
Doug Kearns <dougkearns@gmail.com>
2019-03-13 17:52:45 +1100

    Add rudimentary syntax highlighter testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ Vimrunner::RSpec.configure do |config|
   config.start_vim do
     vim = Vimrunner.start_gvim
     vim.prepend_runtimepath(File.expand_path('../..', __FILE__))
+    vim.add_plugin(File.expand_path('../vim', __FILE__), 'plugin/syntax_test.vim')
     vim.set 'expandtab'
     vim.set 'shiftwidth', 2
     vim
@@ -22,5 +23,15 @@ Vimrunner::RSpec.configure do |config|
     vim.write
 
     expect(IO.read(filename)).to eq string
+  end
+
+  def assert_correct_highlighting(extension='rb', string, pattern, group)
+    filename = "test.#{extension}"
+
+    IO.write filename, string
+
+    vim.edit filename
+
+    expect(vim.echo("TestSyntax('#{pattern}', '#{group}')")).to eq '1'
   end
 end
